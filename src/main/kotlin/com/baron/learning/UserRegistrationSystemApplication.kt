69349d80fcd9a987b2207415249bad1378b9740b
baron <baron@vinova.sg>
2018-12-02 11:33:51 +0700

    first commit

---
+++ src/main/kotlin/com/baron/learning/UserRegistrationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.baron.learning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserRegistrationSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserRegistrationSystemApplication>(*args)
+}
